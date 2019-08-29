3bcd8cf74bc1739cec74924b8606d3b7845dda50
msuphap <suphap.ds@gmail.com>
2019-08-28 20:52:20 -0400

    Grocery List Manager V1

--- src/app/grocery/grocery.component.ts
+++ src/app/grocery/grocery.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-grocery',
   templateUrl: './grocery.component.html',
-  styleUrls: ['./grocery.component.css']
+  styleUrls: ['../../assets/css/bootstrap.min.css']
 })
-export class GroceryComponent implements OnInit {
+export class GroceryComponent {
 
-  constructor() { }
+  task = {
+    name: '',
+    id: 0
+  };
+  tasks = [];
 
-  ngOnInit() {
+  onClick() {
+    if (this.task.id == 0) {
+      this.tasks.push({ id: (new Date()).getTime(), name: this.task.name, strike: false });
+    }
+
+    this.task = {
+      name: '',
+      id: 0
+    };
   }
 
+  onDelete(item) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        this.tasks.splice(i, 1);
+        break;
+      }
+    }
+  }
+
+  onEdit(item) {
+    this.task = item;
+  }
+
+  onStrike(item) {
+    for (var i = 0; i < this.tasks.length; i++) {
+      if (item.id == this.tasks[i].id) {
+        if (this.tasks[i].strike) {
+          this.tasks[i].strike = false;
+        }
+        else {
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }
 }
