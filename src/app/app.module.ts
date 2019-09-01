bd9a9c035fe85ce55ee8d4913ddfe2b80a043a98
msuphap <suphap.ds@gmail.com>
2019-09-01 14:30:55 -0400

    Deleted unused picture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { GroceryComponent } from './grocery/grocery.component';
 import { FormsModule } from '@angular/forms'; 
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    GroceryComponent,
-    AppComponent
+    GroceryComponent
   ],
   imports: [
     BrowserModule,
